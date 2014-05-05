7392b02a66395431a6c6774bdf034fb23c952290
liupeng <79144876@qq.com>
2014-05-05 18:23:23 +0800

    test or

--- ZXingStoreApp/MainPage.xaml.cs
+++ ZXingStoreApp/MainPage.xaml.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Devices.Enumeration;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -33,11 +34,21 @@ namespace ZXingStoreApp
         private readonly MediaCapture _mediaCapture = new MediaCapture();
         private Result _result;
         private bool _navBack;
+        private bool m_bRecording;
+        private bool m_bSuspended;
+        private bool m_bPreviewing;
+        private bool m_bEffectAddedToRecord = false;
+        private bool m_bEffectAddedToPhoto = false;
+        private EventHandler<Object> m_soundLevelHandler;
+        private bool m_bRotateVideoOnOrientationChange;
+        private bool m_bReversePreviewRotation;
+        private DeviceInformationCollection m_devInfoCollection;
         public MainPage()
         {
             this.InitializeComponent();
 
             this.NavigationCacheMode = NavigationCacheMode.Required;
+            EnumerateWebcamsAsync();
         }
 
         /// <summary>
@@ -47,9 +58,54 @@ namespace ZXingStoreApp
         /// This parameter is typically used to configure the page.</param>
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
+            Windows.Graphics.Display.DisplayProperties.OrientationChanged += DisplayProperties_OrientationChanged;
             InitCameraPreviewAction();
         }
 
+        protected override async void OnNavigatingFrom(NavigatingCancelEventArgs e)
+        {
+            Windows.Graphics.Display.DisplayProperties.OrientationChanged -= DisplayProperties_OrientationChanged;
+            try
+            {
+                _navBack = true;
+                VideoCapture.Visibility = Visibility.Collapsed;
+                await _mediaCapture.StopPreviewAsync();
+                VideoCapture.Source = null;
+            }
+            catch (Exception exception)
+            {
+                App.WpLog(exception);
+            }
+        }
+        private async void EnumerateWebcamsAsync()
+        {
+            try
+            {
+                m_devInfoCollection = null;
+
+                EnumedDeviceList2.Items.Clear();
+
+                m_devInfoCollection = await DeviceInformation.FindAllAsync(DeviceClass.VideoCapture);
+                if (m_devInfoCollection.Count == 0)
+                {
+                    // ShowStatusMessage("No WebCams found.");
+                }
+                else
+                {
+                    for (int i = 0; i < m_devInfoCollection.Count; i++)
+                    {
+                        var devInfo = m_devInfoCollection[i];
+                        EnumedDeviceList2.Items.Add(devInfo.Name);
+                    }
+                    EnumedDeviceList2.SelectedIndex = 0;
+                    // ShowStatusMessage("Enumerating Webcams completed successfully.");
+                }
+            }
+            catch (Exception e)
+            {
+                // ShowExceptionMessage(e);
+            }
+        }
         private async void InitCameraPreviewAction()
         {
 #if _TestCatpureImage_
@@ -75,6 +131,23 @@ namespace ZXingStoreApp
                 {
                     settings = new MediaCaptureInitializationSettings { VideoDeviceId = cameras[1].Id }; // 0 => front, 1 => back
                 }
+                var chosenDevInfo = m_devInfoCollection[EnumedDeviceList2.SelectedIndex];
+                settings.VideoDeviceId = chosenDevInfo.Id;
+
+                if (chosenDevInfo.EnclosureLocation != null && chosenDevInfo.EnclosureLocation.Panel == Windows.Devices.Enumeration.Panel.Back)
+                {
+                    m_bRotateVideoOnOrientationChange = true;
+                    m_bReversePreviewRotation = false;
+                }
+                else if (chosenDevInfo.EnclosureLocation != null && chosenDevInfo.EnclosureLocation.Panel == Windows.Devices.Enumeration.Panel.Front)
+                {
+                    m_bRotateVideoOnOrientationChange = true;
+                    m_bReversePreviewRotation = true;
+                }
+                else
+                {
+                    m_bRotateVideoOnOrientationChange = false;
+                }
                 await _mediaCapture.InitializeAsync(settings);
                 SetResolution();
                 //set flash close
@@ -91,14 +164,15 @@ namespace ZXingStoreApp
                 }
 
                 await _mediaCapture.StartPreviewAsync();
+                var currentRotation = GetCurrentPhotoRotation();
                 var count = 0;
                 while (_result == null && !_navBack)
                 {
                     count++;
                     App.WpLog(count);
-                    var photoStorageFile = await KnownFolders.PicturesLibrary.CreateFileAsync("scan.jpg", CreationCollisionOption.GenerateUniqueName);
-                    await _mediaCapture.CapturePhotoToStorageFileAsync(ImageEncodingProperties.CreateJpeg(), photoStorageFile);
-
+                    var tempPhotoStorageFile = await KnownFolders.PicturesLibrary.CreateFileAsync("scan.jpg", CreationCollisionOption.GenerateUniqueName);
+                    await _mediaCapture.CapturePhotoToStorageFileAsync(ImageEncodingProperties.CreateJpeg(), tempPhotoStorageFile);
+                    var photoStorageFile = ReencodePhotoAsync(tempPhotoStorageFile, currentRotation).Result;
                     var stream = await photoStorageFile.OpenReadAsync();
                     // initialize with 1,1 to get the current size of the image
                     var writeableBmp = new WriteableBitmap(1, 1);
@@ -126,9 +200,55 @@ namespace ZXingStoreApp
                 canUseCamera = false;
             }
             if (canUseCamera || _navBack) return;
-          //tips error 
+            //tips error 
         }
+        private async Task<Windows.Storage.StorageFile> ReencodePhotoAsync(
+          Windows.Storage.StorageFile tempStorageFile,
+          Windows.Storage.FileProperties.PhotoOrientation photoRotation)
+        {
+            Windows.Storage.Streams.IRandomAccessStream inputStream = null;
+            Windows.Storage.Streams.IRandomAccessStream outputStream = null;
+            Windows.Storage.StorageFile photoStorage = null;
 
+            try
+            {
+                inputStream = await tempStorageFile.OpenAsync(Windows.Storage.FileAccessMode.Read);
+
+                var decoder = await Windows.Graphics.Imaging.BitmapDecoder.CreateAsync(inputStream);
+
+                photoStorage = await Windows.Storage.KnownFolders.PicturesLibrary.CreateFileAsync("scan.jpg", Windows.Storage.CreationCollisionOption.GenerateUniqueName);
+
+                outputStream = await photoStorage.OpenAsync(Windows.Storage.FileAccessMode.ReadWrite);
+
+                outputStream.Size = 0;
+
+                var encoder = await Windows.Graphics.Imaging.BitmapEncoder.CreateForTranscodingAsync(outputStream, decoder);
+
+                var properties = new Windows.Graphics.Imaging.BitmapPropertySet();
+                properties.Add("System.Photo.Orientation",
+                    new Windows.Graphics.Imaging.BitmapTypedValue(photoRotation, Windows.Foundation.PropertyType.UInt16));
+
+                await encoder.BitmapProperties.SetPropertiesAsync(properties);
+
+                await encoder.FlushAsync();
+            }
+            finally
+            {
+                if (inputStream != null)
+                {
+                    inputStream.Dispose();
+                }
+
+                if (outputStream != null)
+                {
+                    outputStream.Dispose();
+                }
+
+                var asyncAction = tempStorageFile.DeleteAsync(Windows.Storage.StorageDeleteOption.PermanentDelete);
+            }
+
+            return photoStorage;
+        }
         private async System.Threading.Tasks.Task DecodeStaticResource()
         {
 #if _TestCatpureImage_
@@ -178,10 +298,86 @@ namespace ZXingStoreApp
 
             return result;
         }
+        private void PrepareForVideoRecording()
+        {
+            if (_mediaCapture == null)
+            {
+                return;
+            }
+
+            bool counterclockwiseRotation = m_bReversePreviewRotation;
+
+            if (m_bRotateVideoOnOrientationChange)
+            {
+                _mediaCapture.SetRecordRotation(VideoRotationLookup(Windows.Graphics.Display.DisplayProperties.CurrentOrientation, counterclockwiseRotation));
+            }
+            else
+            {
+                _mediaCapture.SetRecordRotation(Windows.Media.Capture.VideoRotation.None);
+            }
+        }
+        private Windows.Storage.FileProperties.PhotoOrientation GetCurrentPhotoRotation()
+        {
+            bool counterclockwiseRotation = m_bReversePreviewRotation;
+
+            if (m_bRotateVideoOnOrientationChange)
+            {
+                return PhotoRotationLookup(Windows.Graphics.Display.DisplayProperties.CurrentOrientation, counterclockwiseRotation);
+            }
+            else
+            {
+                return Windows.Storage.FileProperties.PhotoOrientation.Normal;
+            }
+        }
+        private void DisplayProperties_OrientationChanged(object sender)
+        {
+            if (_mediaCapture == null)
+            {
+                return;
+            }
+
+            bool previewMirroring = _mediaCapture.GetPreviewMirroring();
+            bool counterclockwiseRotation = (previewMirroring && !m_bReversePreviewRotation) ||
+                (!previewMirroring && m_bReversePreviewRotation);
+
+            if (m_bRotateVideoOnOrientationChange)
+            {
+                _mediaCapture.SetPreviewRotation(VideoRotationLookup(Windows.Graphics.Display.DisplayProperties.CurrentOrientation, counterclockwiseRotation));
+            }
+            else
+            {
+                _mediaCapture.SetPreviewRotation(Windows.Media.Capture.VideoRotation.None);
+            }
+        }
+
+        private Windows.Storage.FileProperties.PhotoOrientation PhotoRotationLookup(
+            Windows.Graphics.Display.DisplayOrientations displayOrientation,
+            bool counterclockwise)
+        {
+            switch (displayOrientation)
+            {
+                case Windows.Graphics.Display.DisplayOrientations.Landscape:
+                    return Windows.Storage.FileProperties.PhotoOrientation.Normal;
+
+                case Windows.Graphics.Display.DisplayOrientations.Portrait:
+                    return (counterclockwise) ? Windows.Storage.FileProperties.PhotoOrientation.Rotate270 :
+                        Windows.Storage.FileProperties.PhotoOrientation.Rotate90;
+
+                case Windows.Graphics.Display.DisplayOrientations.LandscapeFlipped:
+                    return Windows.Storage.FileProperties.PhotoOrientation.Rotate180;
+
+                case Windows.Graphics.Display.DisplayOrientations.PortraitFlipped:
+                    return (counterclockwise) ? Windows.Storage.FileProperties.PhotoOrientation.Rotate90 :
+                        Windows.Storage.FileProperties.PhotoOrientation.Rotate270;
+
+                default:
+                    return Windows.Storage.FileProperties.PhotoOrientation.Unspecified;
+            }
+        }
 
         private Windows.Media.Capture.VideoRotation VideoRotationLookup(
-         Windows.Graphics.Display.DisplayOrientations displayOrientation,
-         bool counterclockwise)
+            Windows.Graphics.Display.DisplayOrientations displayOrientation,
+            bool counterclockwise)
         {
             switch (displayOrientation)
             {
@@ -203,6 +399,7 @@ namespace ZXingStoreApp
                     return Windows.Media.Capture.VideoRotation.None;
             }
         }
+
         //This is how you can set your resolution
         public async void SetResolution()
         {
@@ -228,20 +425,6 @@ namespace ZXingStoreApp
             }
         }
 
-        protected override async void OnNavigatingFrom(NavigatingCancelEventArgs e)
-        {
-            try
-            {
-                _navBack = true;
-                VideoCapture.Visibility = Visibility.Collapsed;
-                await _mediaCapture.StopPreviewAsync();
-                VideoCapture.Source = null;
-            }
-            catch (Exception exception)
-            {
-                App.WpLog(exception);
-            }
-        }
 
     }
 }
